97bddc9f82fdb62df4ef43ac33301d34c3389eeb
Courtney Miyashiro <59979452+c0deney@users.noreply.github.com>
2020-02-26 11:48:05 -0500

    Update FizzBuzz_test.py

--- Python/FizzBuzz_test.py
+++ Python/FizzBuzz_test.py
@@ -2,7 +2,7 @@
 """Unit tests for FizzBuzz."""
 
 # import the code to be tested
-from fizzbuzz import fizz
+from fizzbuzz import fizz, buzz, fibu
 
 #import the method needed to test for exceptions
 from pytest import raises
@@ -17,16 +17,71 @@ def describe_a_fizzbuzz_program_that():
   def describes_a_fizz_function_that():
     """Tests for the fizz() function"""
 
-    def throws_an_error_if_input_is_not_a_postive_integer_or_text():
-      with raises(TypeError) as err_info:
+    def throws_an_error_if_no_input():
+      with raises(Exception) as err_info:
         fizz() # pylint: disable=no-value-for-parameter
+      assert err_info.type == TypeError
+      assert 'missing 1 required positional argument' in str(err_info.value)
 
-    def can_determine_if_a_number_is_a_multiple_of_3():
-      """Checks to see if a given number is a multiple of 3."""
-      assert fizz(3) == True       # multiple of 3
-      assert fizz(2) == False      # non-multiple of 3
-      assert fizz(3.5) == False    # non-integer
-      assert fizz(0) == True       # zero
-      assert fizz(-3) == True      # negative multiple of 3
-      assert fizz(-4) == False     # negative non-multiple of 3
-      assert fizz('buzz') == False # non-numeric input
+    def returns_fizz_if_a_number_is_a_multiple_of_3():
+      """
+        Takes an input `x` and checks to see if it is a
+        number, and if so, also a multiple of 3.
+        If it is both, return 'fizz'.
+        Otherwise, return the input.
+      """
+      assert fizz(3) == 'fizz'      # multiple of 3
+      assert fizz(2) == 2           # non-multiple of 3
+      assert fizz(3.5) == 3.5       # non-integer
+      assert fizz(0) == 'fizz'      # zero
+      assert fizz(-3) == 'fizz'     # negative multiple of 3
+      assert fizz(-4) == -4         # negative non-multiple of 3
+      assert fizz('buzz') == 'buzz' # non-numeric input
+
+  def describes_a_buzz_function_that():
+    """Tests for the buzz() function"""
+
+    def throws_an_error_if_no_input():
+      with raises(Exception) as err_info:
+        buzz() # pylint: disable=no-value-for-parameter
+      assert err_info.type == TypeError
+      assert 'missing 1 required positional argument' in str(err_info.value)
+
+    def returns_buzz_if_a_number_is_a_multiple_of_5():
+      """
+        Takes an input `x` and checks to see if it is a
+        number, and if so, also a multiple of 5.
+        If it is both, return 'buzz'.
+        Otherwise, return the input.
+      """
+      assert buzz(5) == 'buzz'      # multiple of 5
+      assert buzz(2) == 2           # non-multiple of 5
+      assert buzz(5.5) == 5.5       # non-integer
+      assert buzz(0) == 'buzz'      # zero
+      assert buzz(-5) == 'buzz'     # negative multiple of 5
+      assert buzz(-4) == -4         # negative non-multiple of 5
+      assert buzz('fizz') == 'fizz' # non-numeric input
+
+  def describes_a_fibu_function_that():
+    """Tests for the fibu() function"""
+
+    def throws_an_error_if_no_input():
+      with raises(Exception) as err_info:
+        fibu() # pylint: disable=no-value-for-parameter
+      assert err_info.type == TypeError
+      assert 'missing 1 required positional argument' in str(err_info.value)
+
+    def returns_fizzbuzz_if_a_number_is_a_multiple_of_15():
+      """
+        Takes an input `x` and checks to see if it is a
+        number, and if so, also a multiple of 15.
+        If it is both, return 'fizzbuzz'.
+        Otherwise, return the input.
+      """
+      assert fibu(15) == 'fizzbuzz'  # multiple of 15
+      assert fibu(2) == 2            # non-multiple of 15
+      assert fibu(5.5) == 5.5        # non-integer
+      assert fibu(0) == 'fizzbuzz'   # zero
+      assert fibu(-15) == 'fizzbuzz' # negative multiple of 15
+      assert fibu(-4) == -4          # negative non-multiple of 15
+      assert fibu('fizz') == 'fizz'  # non-numeric input
