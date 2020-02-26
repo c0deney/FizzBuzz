06fdc55750b0ea56fca89542c6984b04e4609888
Courtney Miyashiro <59979452+c0deney@users.noreply.github.com>
2020-02-26 12:02:39 -0500

    Update FizzBuzz_test.py

--- Python/FizzBuzz_test.py
+++ Python/FizzBuzz_test.py
@@ -2,9 +2,9 @@
 """Unit tests for FizzBuzz."""
 
 # import the code to be tested
-from fizzbuzz import fizz, buzz, fibu
+from fizzbuzz import fizz, buzz, fibu, play_fizzbuzz
 
-#import the method needed to test for exceptions
+# import method to test errors
 from pytest import raises
 
 def describe_a_fizzbuzz_program_that():
@@ -14,74 +14,77 @@ def describe_a_fizzbuzz_program_that():
     """Make sure our testing infrastructure is working."""
     assert True == True
 
-  def describes_a_fizz_function_that():
-    """Tests for the fizz() function"""
+  def describes_a_function_fizz_that():
+    """Tests related to our fizz() function"""
 
     def throws_an_error_if_no_input():
-      with raises(Exception) as err_info:
+      with raises(Exception) as exception_info:
         fizz() # pylint: disable=no-value-for-parameter
-      assert err_info.type == TypeError
-      assert 'missing 1 required positional argument' in str(err_info.value)
+      assert exception_info.type == TypeError
+      assert "missing 1 required positional argument" in str(exception_info.value)
 
-    def returns_fizz_if_a_number_is_a_multiple_of_3():
+    def returns_fizz_if_x_is_multiple_of_3():
       """
-        Takes an input `x` and checks to see if it is a
-        number, and if so, also a multiple of 3.
-        If it is both, return 'fizz'.
-        Otherwise, return the input.
+        Checks to see if an input is a multiple of 3.
+        Returns 'Fizz' if it is.
+        Returns the input if it is not.
       """
-      assert fizz(3) == 'fizz'      # multiple of 3
+      assert fizz(3) == 'Fizz'      # multiple of 3
       assert fizz(2) == 2           # non-multiple of 3
-      assert fizz(3.5) == 3.5       # non-integer
-      assert fizz(0) == 'fizz'      # zero
-      assert fizz(-3) == 'fizz'     # negative multiple of 3
+      assert fizz(0) == 'Fizz'      # zero
+      assert fizz(-3) == 'Fizz'     # negative multiple of 3
       assert fizz(-4) == -4         # negative non-multiple of 3
-      assert fizz('buzz') == 'buzz' # non-numeric input
+      assert fizz('Buzz') == 'Buzz' # non-number input
 
-  def describes_a_buzz_function_that():
-    """Tests for the buzz() function"""
+  def describes_a_function_buzz_that():
+    """Tests related to our buzz() function"""
 
     def throws_an_error_if_no_input():
-      with raises(Exception) as err_info:
+      with raises(Exception) as exception_info:
         buzz() # pylint: disable=no-value-for-parameter
-      assert err_info.type == TypeError
-      assert 'missing 1 required positional argument' in str(err_info.value)
+      assert exception_info.type == TypeError
+      assert "missing 1 required positional argument" in str(exception_info.value)
 
-    def returns_buzz_if_a_number_is_a_multiple_of_5():
+    def returns_buzz_if_x_is_multiple_of_5():
       """
-        Takes an input `x` and checks to see if it is a
-        number, and if so, also a multiple of 5.
-        If it is both, return 'buzz'.
-        Otherwise, return the input.
+        Checks to see if an input is a multiple of 5.
+        Returns 'Buzz' if it is.
+        Returns the input if it is not.
       """
-      assert buzz(5) == 'buzz'      # multiple of 5
+      assert buzz(5) == 'Buzz'      # multiple of 5
       assert buzz(2) == 2           # non-multiple of 5
-      assert buzz(5.5) == 5.5       # non-integer
-      assert buzz(0) == 'buzz'      # zero
-      assert buzz(-5) == 'buzz'     # negative multiple of 5
+      assert buzz(0) == 'Buzz'      # zero
+      assert buzz(-5) == 'Buzz'     # negative multiple of 5
       assert buzz(-4) == -4         # negative non-multiple of 5
-      assert buzz('fizz') == 'fizz' # non-numeric input
+      assert buzz('Fizz') == 'Fizz' # non-number input
 
-  def describes_a_fibu_function_that():
-    """Tests for the fibu() function"""
+  def describes_a_function_fibu_that():
+    """Tests related to our fibu() function"""
 
     def throws_an_error_if_no_input():
-      with raises(Exception) as err_info:
+      with raises(Exception) as exception_info:
         fibu() # pylint: disable=no-value-for-parameter
-      assert err_info.type == TypeError
-      assert 'missing 1 required positional argument' in str(err_info.value)
+      assert exception_info.type == TypeError
+      assert "missing 1 required positional argument" in str(exception_info.value)
 
-    def returns_fizzbuzz_if_a_number_is_a_multiple_of_15():
+    def returns_fizzbuzz_if_x_is_multiple_of_15():
       """
-        Takes an input `x` and checks to see if it is a
-        number, and if so, also a multiple of 15.
-        If it is both, return 'fizzbuzz'.
-        Otherwise, return the input.
+        Checks to see if an input is a multiple of 15.
+        Returns 'FizzBuzz' if it is.
+        Returns the input if it is not.
       """
-      assert fibu(15) == 'fizzbuzz'  # multiple of 15
+      assert fibu(15) == 'FizzBuzz'  # multiple of 15
       assert fibu(2) == 2            # non-multiple of 15
-      assert fibu(5.5) == 5.5        # non-integer
-      assert fibu(0) == 'fizzbuzz'   # zero
-      assert fibu(-15) == 'fizzbuzz' # negative multiple of 15
+      assert fibu(0) == 'FizzBuzz'   # zero
+      assert fibu(-15) == 'FizzBuzz' # negative multiple of 15
       assert fibu(-4) == -4          # negative non-multiple of 15
-      assert fibu('fizz') == 'fizz'  # non-numeric input
+      assert fibu('Fizz') == 'Fizz'  # non-number input
+
+  def can_play_fizzbuzz():
+    """
+      Demonstrate that we can generate the correct output
+      for a minimal test case of the actual FizzBuzz game.
+    """
+    assert play_fizzbuzz(1, 15) == [
+      1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7 , 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'
+    ]
