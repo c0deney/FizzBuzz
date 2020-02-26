2860533e8e720136f1090308d346d7898d3add3c
Courtney Miyashiro <59979452+c0deney@users.noreply.github.com>
2020-02-26 11:53:49 -0500

    Update FizzBuzz.py

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -11,3 +11,35 @@ for x in range(1,101):
     print("Buzz")
   else:
     print(x)
+    
+"""A FizzBuzz program"""
+
+# import necessary supporting libraries or packages
+from numbers import Number
+
+def fizz(x):
+  """
+    Takes an input `x` and checks to see if x is a
+    number, and if so, also a multiple of 3.
+    If it is both, return 'fizz'.
+    Otherwise, return the input.
+  """
+  return 'fizz' if isinstance(x, Number) and x % 3 == 0 else x
+
+def buzz(x):
+  """
+    Takes an input `x` and checks to see if x is a
+    number, and if so, also a multiple of 5.
+    If it is both, return 'buzz'.
+    Otherwise, return the input.
+  """
+  return 'buzz' if isinstance(x, Number) and x % 5 == 0 else x
+
+def fibu(x):
+  """
+    Takes an input `x` and checks to see if x is a
+    number, and if so, also a multiple of 15.
+    If it is both, return 'fizzbuzz'.
+    Otherwise, return the input.
+  """
+  return 'fizzbuzz' if isinstance(x, Number) and x % 15 == 0 else x
