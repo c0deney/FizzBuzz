ae356eb5c0d4e41f6a8043f31b2b433f50f730bf
Courtney Miyashiro <59979452+c0deney@users.noreply.github.com>
2020-02-26 11:48:47 -0500

    Update FizzBuzz.py

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -1,13 +1,13 @@
 #fizzbuzz1
 
-i = 0
+x = 0
 
-for i in range(1,100):
-  if (i % 3 == 0) and (i % 5 == 0):
+for x in range(1,101):
+  if (x % 3 == 0) and (x % 5 == 0):
     print("Fizzbuzz")
-  elif i % 3 == 0:
+  elif x % 3 == 0:
     print("Fizz")
-  elif i % 5 == 0:
+  elif x % 5 == 0:
     print("Buzz")
   else:
-    print(i)
+    print(x)
